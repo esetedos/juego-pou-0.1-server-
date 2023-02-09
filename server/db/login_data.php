043b2b2fefb2714d26312bf457f6974bb2eb4a26
IKER MENDOZA LABORRAgit config --global user.email iker.mendoza@ikasle.aeg.eus <iker.mendoza@ikasle.aeg.eus>
2023-02-09 13:52:38 +0100

    nombre de la base de datos cambiada

--- server/db/login_data.php
+++ server/db/login_data.php
@@ -1,7 +1,7 @@
 <?php
 
     //Datos de conexion con la base de datos
-    $cfg['nombre_bd']   = 'arcade';
+    $cfg['nombre_bd']   = 'dbrehqurdnz3gg';
     $cfg['servidor']    = 'localhost';
     $cfg['usuario']     = 'ua73mvsne6m82';
     $cfg['password']    = 'satellite.invaders';
