f2ebc62665ee11274545a194cfd32adb95e581a0
IKER MENDOZA LABORRAgit config --global user.email iker.mendoza@ikasle.aeg.eus <iker.mendoza@ikasle.aeg.eus>
2023-02-09 08:14:19 +0100

    Usuario y servidor cambiados para el Cpanel

--- server/db/login_data.php
+++ server/db/login_data.php
@@ -3,7 +3,7 @@
     //Datos de conexion con la base de datos
     $cfg['nombre_bd']   = 'arcade';
     $cfg['servidor']    = 'localhost';
-    $cfg['usuario']     = 'root';
-    $cfg['password']    = '';
-// satellite.invaders
+    $cfg['usuario']     = 'ua73mvsne6m82';
+    $cfg['password']    = 'satellite.invaders';
+
 ?>
